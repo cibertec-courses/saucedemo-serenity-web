4c1cc3a724f3e30ab18856d7b3aefd9124e2aa3b
Christiam Mendoza <ch.alb.rz@gmail.com>
2025-12-03 19:55:13 -0500

    Login Steps OK

---
+++ src/test/java/com/testing/steps/LoginSteps.java
@@ -0,0 +1,49 @@
+package com.testing.steps;
+
+
+import com.testing.pages.LoginPage;
+import com.testing.pages.ProductsPage;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import net.serenitybdd.annotations.Managed;
+import org.openqa.selenium.WebDriver;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoginSteps {
+
+    @Managed
+    WebDriver driver;
+
+
+    LoginPage loginPage;
+    ProductsPage productsPage;
+
+    @Given("el usuario esta en la pagina de login")
+    public void elUsuarioEstaEnLaPaginaDeLogin(){
+        loginPage.openPage();
+    }
+
+    @When("ingresa el usuario {string}")
+    public void ingresElUsuario(String username){
+        loginPage.enterUserName(username);
+    }
+
+    @And("ingresar la contraseña {string}")
+    public void ingresaLaContrasena(String password){
+        loginPage.enterPassword(password);
+    }
+
+    @And("hace clic en el boton login")
+    public void haceClicEnBotonLogin(){
+        loginPage.clickLogin();
+    }
+
+    @Then("debe ver la pagina de productos")
+    public  void debeVerLaPaginaDeProductos (){
+        assertTrue(productsPage.isDisplayed());
+    }
+
+}
